5f06372f8308e1b087b75cdc994addddd8420055
kolisovet <kolisovet@gmail.com>
2022-12-12 00:36:20 +0700

    14 les

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,22 +1,23 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
         <div>
             <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'></img>
         </div>
         <div>
             ava + Disc
         </div>
-        <div>
+        <div className={s.posts}>
             my posts
-            <div>
+            <div className={s.item}>
                 New post
             </div>
-            <div>
+            <div className={s.item}>
                 post 1
             </div>
-            <div>
+            <div className={s.item}>
                 post 2
             </div>
         </div>
