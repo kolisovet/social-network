0839afefc5c6c40d88b9ccd05273b469e11dd63a
kolisovet <kolisovet@gmail.com>
2022-12-11 17:09:44 +0700

    first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
+      <div>
+              <Header />
     <div className="App">
      Hello, samurai! Let's go!
     </div>
+      </div>
   );
 }
 
+const Header = () => {
+    return (
+        <div className="Header">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </div>
+    )
+}
 export default App;
