54e7cd2c37b9b167ab86245d64c8647dbd86d87c
kolisovet <kolisovet@gmail.com>
2023-01-24 15:31:42 +0700

    20 les

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to='/profile' className={({isActive}) => isActive ? 'color: gold'  : 'color: white'}>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/dialogs'>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/news'>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/music'>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/settings'>Settings</NavLink>
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
